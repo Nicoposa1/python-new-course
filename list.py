8068e0baa2096be5498469df2c54c8d9baa82925
Nicolas Posa <nicoposa57@gmail.com>
2025-01-11 04:27:55 -0300

    slice method

--- list.py
+++ list.py
@@ -1,12 +1,27 @@
-to_do = ["study", "eat", "sleep", "repeat"]
-print(to_do)
-numbers = [1, 2, 3, 4, "cinco", True, 6.7]
-print(type(numbers))
-print(len(numbers))
+# to_do = ["study", "eat", "sleep", "repeat"]
+# print(to_do)
+# numbers = [1, 2, 3, 4, "cinco", True, 6.7]
+# print(type(numbers))
+# print(len(numbers))
 
-numbers.append(False)
-print(numbers)
-print(numbers.index(3))
+# numbers.append(False)
+# print(numbers)
+# print(numbers.index(3))
 
-del numbers[-2]
-print(numbers)
+# del numbers[-2]
+# print(numbers)
+
+a = [1, 2, 3, 4, 5]
+b = a 
+del a[0]
+print(a)
+print(b)
+c = a[:]
+print(id(a))
+print(id(b))
+print(id(c)) 
+a.append(6)
+print(a)
+print(b)
+print(c)
+
